ce85c8c6675087294f210c2538c0d22691edc2d1
Sokunbi Yusuf <adeolaoo87@gmail.com>
2024-07-11 06:53:28 +0100

    html first page

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-import { apiKey } from "./apikey.js";
+
+
+let productHtml = "";
+
+videos.forEach((video) => {
+	// console.log(video)
+ productHtml += `
+  <div class="product-container">
+  <div class="product-image-container">
+	<a href="artice.html" target="_blank"><iframe class="frame-video"
+    src="https://www.youtube.com/embed/${video.id}">
+			</iframe></a>
+</div>
+  <div class="product-name limit-text-to-2-lines linkvideo">
+	<div><a href="artice.html" target="_blank"></a></div>
+   
+  </div>
+	
+  <div class="product-name limit-text-to-2-lines linkvideo">
+	${video.title}
+   
+  </div>
+
+  <div class="product-rating-container">
+	${video.views}. views
+    </div>
+
+  <div class="product-price">
+	${video.duration}s
+  </div>
+	<div>${video.uploaded}</div>
+	<div><a href="${channels.link}" target="_blank">click here</a></div>
+	</div>
+	
+ `;
+});
+
+document.querySelector('.display').innerHTML = productHtml;
+
---
+++ object.js
@@ -0,0 +1,397 @@
+ const videos = [
+      {
+          id: "6nchgV1Gt3E",
+          title: "Dj Baddo Best Of 9ice Mix",
+          link: "https://youtu.be/6nchgV1Gt3E",
+          thumbnail: "https://i.ytimg.com/vi/6nchgV1Gt3E/hqdefault.jpg",
+          channel: {
+              "id": "UCMj9mDMge266eFvUGRRHrXQ",
+              "name": "Dj Baddo",
+              "link": "https://www.youtube.com/channel/UCMj9mDMge266eFvUGRRHrXQ",
+              "handle": null,
+              "verified": true,
+              "thumbnail": "https://yt3.ggpht.com/oyE4Ikpm0coDAkUSIUfaNYnASneYdgZUPzlvWjUKOIfqtQ91tm_T2ZuCNVuD6gdAfWw0nmauxLA=s0?imgmax=0"
+         
+     },
+          description: "",
+          views: 286356,
+          uploaded: "vor 5 Jahren",
+          duration: 4361,
+          durationString: "1:12:41"
+        },
+     {
+                id: "hBRaNVEsft8",
+                title: "9ice - Living Things (Official Video)",
+                link: "https://youtu.be/hBRaNVEsft8",
+                "thumbnail": "https://i.ytimg.com/vi/hBRaNVEsft8/hqdefault.jpg",
+                "channel": {
+                    "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+                    "name": "9ICEOFFICIAL",
+                    "link": "https://www.youtube.com/channel/UCtggJrgsA8cYpUPXG4_SvKQ",
+                    "handle": null,
+                    "verified": true,
+                    "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0"
+                },
+                "description": "Subscribe to 9ICE TV Official YouTube Channel: http://goo.gl/c4ZVOI.",
+                "views": 6192675,
+                "uploaded": "vor 7 Jahren",
+                "duration": 250,
+                "durationString": "4:10"
+            }
+            ,
+            {
+                "id": "z5u6eYq-UtA",
+                "title": "9ice - Gongo Aso",
+                "link": "https://youtu.be/z5u6eYq-UtA",
+                "thumbnail": "https://i.ytimg.com/vi/z5u6eYq-UtA/hqdefault.jpg",
+                "channel": {
+                    "id": "UCDj3OK-Cpy__7oLFwvTcpaA",
+                    "name": "Ayo Adeniyi",
+                    "link": "https://www.youtube.com/@ayodigital",
+                    "handle": "@ayodigital",
+                    "verified": false,
+                    "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_lAchCsNMSC5v2y7okxv2TFZZTOzNTIG8BB4s-eXPzU2Vo=s0?imgmax=0"
+                }, "description": "Telly Media are a Lagos, Nigeria based video production company & equipment rental that enables new filmmaker and ...",
+                "views": 1324526,
+                "uploaded": "vor 14 Jahren",
+                "duration": 227,
+                "durationString": "3:47"
+            },
+             
+      {
+        "id": "h5U72nExU2U",
+        "title": "9ice ft 2Face Idibia - Street Credibility",
+        "link": "https://youtu.be/h5U72nExU2U",
+        "thumbnail": "https://i.ytimg.com/vi/h5U72nExU2U/hqdefault.jpg",
+        "channel": {
+            "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+            "name": "9ICEOFFICIAL",
+            "link": "https://www.youtube.com/channel/UCtggJrgsA8cYpUPXG4_SvKQ",
+            "handle": null,
+            "verified": true,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0"
+        },
+        "description": "Subscribe to 9ICE TV Official YouTube Channel: http://goo.gl/c4ZVOI.",
+        "views": 306799,
+        "uploaded": "vor 2 Jahren",
+        "duration": 267,
+        "durationString": "4:27"
+    },        
+    {
+        "id": "ZtlVfVVpk-0",
+        "title": "Photocopy",
+        "link": "https://youtu.be/ZtlVfVVpk-0",
+        "thumbnail": "https://i.ytimg.com/vi/ZtlVfVVpk-0/hqdefault.jpg",
+        "channel": {
+            "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+            "name": "9ICEOFFICIAL",
+            "link": "https://www.youtube.com/channel/UCtggJrgsA8cYpUPXG4_SvKQ",
+            "handle": null,
+            "verified": true,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0"
+        },
+        "description": "Provided to YouTube by IIP-DDS Photocopy · 9ice Gongo Aso ℗ AAR Released on: 2008-03-15 Associated Performer: Alexander ...",
+        "views": 106098,
+        "uploaded": "",
+        "duration": 263,
+        "durationString": "4:23"
+    },
+    {
+        "id": "6sPnZGttx-w",
+        "title": "9ice -  Ayepo Gan (Official Video)",
+        "link": "https://youtu.be/6sPnZGttx-w",
+        "thumbnail": "https://i.ytimg.com/vi/6sPnZGttx-w/hqdefault.jpg",
+        "channel": {
+            "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+            "name": "9ICEOFFICIAL",
+            "link": "https://www.youtube.com/channel/UCtggJrgsA8cYpUPXG4_SvKQ",
+            "handle": null,
+            "verified": true,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0"
+        },
+        "description": "Subscribe to 9ICE Official YouTube Channel: http://goo.gl/c4ZVOI -- #9ICE #Ancestor #CAUTIONTV #Nigeriamusic ...",
+        "views": 222348,
+        "uploaded": "vor 4 Jahren",
+        "duration": 217,
+        "durationString": "3:37"
+    },
+    {
+        "id": "mcGQcaYDbJ8",
+        "title": "9ICE - MERCY (OFFICIAL VIDEO)",
+        "link": "https://youtu.be/mcGQcaYDbJ8",
+        "thumbnail": "https://i.ytimg.com/vi/mcGQcaYDbJ8/hqdefault.jpg",
+        "channel": {
+            "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+            "name": "9ICEOFFICIAL",
+            "link": "https://www.youtube.com/channel/UCtggJrgsA8cYpUPXG4_SvKQ",
+            "handle": null,
+            "verified": true,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0"
+        },
+        "description": "-- #MERCY #9ICE #Ancestor #CAUTIONTV #tonight #pariboto #tgif #worlmusic #Nigeriamusic #Basirimi #NigerianMusic2018 ...",
+        "views": 810437,
+        "uploaded": "vor 5 Jahren",
+        "duration": 234,
+        "durationString": "3:54"
+    },
+    {
+        id: "_aprv6bpkws",
+        title: "9ice - Gbamu Gbamu (Official Video)new",
+        link: "https://youtu.be/_aprv6bpkws",
+        thumbnail: "https://i.ytimg.com/vi/_aprv6bpkws/hqdefault.jpg",
+        channel: {
+            id: "UCd3e6AMLAlH81syk0TusLog",
+            name: "Oluyemi Sam",
+            link: "https://www.youtube.com/@Oluyemisam",
+            handle: "@Oluyemisam",
+            verified: false,
+            thumbnail: "https://yt3.ggpht.com/ytc/AIdro_l2Opoj8ch_SdrIliaG0xS_Rm2JJ7K9swIfwYW0gyQ=s0?imgmax=0"
+        },
+        description: "This is another one from Mr 9ICE himself..enjoy.",
+        views: 222947,
+        uploaded: "vor 14 Jahren",
+        duration: 244,
+        durationString: "4:04"
+    },
+    {
+        id: "5H1tDTPcaA8",
+        title: "9ice - Everyday (Official Video)",
+        link: "https://youtu.be/5H1tDTPcaA8",
+        thumbnail: "https://i.ytimg.com/vi/5H1tDTPcaA8/hqdefault.jpg",
+        channel: {
+            "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+            "name": "9ICEOFFICIAL",
+            "link": "https://www.youtube.com/channel/UCtggJrgsA8cYpUPXG4_SvKQ",
+            "handle": null,
+            "verified": true,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0"
+        },
+        "description": "Subscribe to 9ICE TV Official YouTube Channel: http://goo.gl/c4ZVOI.",
+        "views": 59705,
+        "uploaded": "vor 2 Jahren",
+        "duration": 229,
+        "durationString": "3:49"
+    },
+    {
+        "id": "q3NPqiZoeVw",
+        "title": "9ICE LIFE DRAMA",
+        "link": "https://youtu.be/q3NPqiZoeVw",
+        "thumbnail": "https://i.ytimg.com/vi/q3NPqiZoeVw/hqdefault.jpg",
+        "channel": {
+            "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+            "name": "9ICEOFFICIAL",
+            "link": "https://www.youtube.com/channel/UCtggJrgsA8cYpUPXG4_SvKQ",
+            "handle": null,
+            "verified": true,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0"
+        },
+        "description": "",
+        "views": 464030,
+        "uploaded": "vor 10 Jahren",
+        "duration": 247,
+        "durationString": "4:07"
+    },
+    {
+        "id": "uxPsxU1XMkk",
+        "title": "9ice - No Be Mistake",
+        "link": "https://youtu.be/uxPsxU1XMkk",
+        "thumbnail": "https://i.ytimg.com/vi/uxPsxU1XMkk/hqdefault.jpg",
+        "channel": {
+            "id": "UC8GuvsHhJu2-hWv6u8BMclg",
+            "name": "9iceTV",
+            "link": "https://www.youtube.com/@9iceTV",
+            "handle": "@9iceTV",
+            "verified": false,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_kK9Tz52EK2XNAmu0ldeoiCPRcRt-sIwT-NdY8NGa5qkw=s0?imgmax=0"
+        },
+        "description": "9ice with No Be Mistake.",
+        "views": 509986,
+        "uploaded": "vor 14 Jahren",
+        "duration": 299,
+        "durationString": "4:59"
+    },
+    {
+        "id": "2ihbVr9n_Xw",
+        "title": "9ICE EPIC PERFORMANCE AT MUSIC RAVE WITH DELE OMO WOLI 2.0",
+        "link": "https://youtu.be/2ihbVr9n_Xw",
+        "thumbnail": "https://i.ytimg.com/vi/2ihbVr9n_Xw/hqdefault.jpg",
+        "channel": {
+            "id": "UC9iUJEDS9L62h1tWylvJazQ",
+            "name": "DELE OMO WOLI",
+            "link": "https://www.youtube.com/@DELEOMOWOLI",
+            "handle": "@DELEOMOWOLI",
+            "verified": false,
+            "thumbnail": "https://yt3.ggpht.com/OQFKBei3KO2Ajd7xqha_O0rQEC9r7S5Oz36aUQvEMP4C8XUEpJiv5nbv32cRV2NRJwMJOyT3hQ=s0?imgmax=0"
+        },
+        "description": "Music Rave with Dele Omo Woli 2.0 #MusicRavewithDeleOmoWoli2.0 #deleomowoli #musicrave #music #concert #dflaming ...",
+        "views": 37722,
+        "uploaded": "vor 3 Wochen",
+        "duration": 880,
+        "durationString": "14:40"
+    },
+    {
+        "id": "FA5lbgy4gbE",
+        "title": "JUNIOR BOY FT 9ICE - IRAPADA 2.0 (dir by UNLIMITED LA)",
+        "link": "https://youtu.be/FA5lbgy4gbE",
+        "thumbnail": "https://i.ytimg.com/vi/FA5lbgy4gbE/hqdefault.jpg",
+        "channel": {
+            "id": "UC8AqbxdsqkiW_ojDMrBbNAw",
+            "name": "Swerve Innercity",
+            "link": "https://www.youtube.com/@SwerveInnercity",
+            "handle": "@SwerveInnercity",
+            "verified": false,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_m8TGQg2QYtwFcgetnIBmqLFG2KXTHiVZzoHS-9tgyPMA=s0?imgmax=0"
+        },
+        "description": "Please Subscribe: https://goo.gl/wlKWt5.",
+        "views": 2743265,
+        "uploaded": "vor 7 Jahren",
+        "duration": 273,
+        "durationString": "4:33"
+    },
+    {
+        "id": "2soDJ7ne5FU",
+        "title": "9ICE - ECONOMY (OFFICIAL VIDEO)",
+        "link": "https://youtu.be/2soDJ7ne5FU",
+        "thumbnail": "https://i.ytimg.com/vi/2soDJ7ne5FU/hqdefault.jpg",
+        "channel": {
+            "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+            "name": "9ICEOFFICIAL",
+            "link": "https://www.youtube.com/channel/UCtggJrgsA8cYpUPXG4_SvKQ",
+            "handle": null,
+            "verified": true,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0"
+        },
+        "description": "Subscribe to 9ICE TV Official YouTube Channel: http://goo.gl/c4ZVOI.",
+        "views": 694373,
+        "uploaded": "vor 7 Jahren",
+        "duration": 239,
+        "durationString": "3:59"
+    },
+    {
+        "id": "xaOXT9R9BNA",
+        "title": "9ICE - BASIRI MI | OFFICIAL VIDEO",
+        "link": "https://youtu.be/xaOXT9R9BNA",
+        "thumbnail": "https://i.ytimg.com/vi/xaOXT9R9BNA/hqdefault.jpg",
+        "channel": {
+            "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+            "name": "9ICEOFFICIAL",
+            "link": "https://www.youtube.com/channel/UCtggJrgsA8cYpUPXG4_SvKQ",
+            "handle": null,
+            "verified": true,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0"
+        },
+        "description": "Subscribe to 9ICE TV Official YouTube Channel: http://goo.gl/c4ZVOI.",
+        "views": 524603,
+        "uploaded": "vor 6 Jahren",
+        "duration": 203,
+        "durationString": "3:23"
+    },
+    {
+        "id": "KMSYcAONMEM",
+        "title": "best of 9ICE mixtape by DJ Jasper",
+        "link": "https://youtu.be/KMSYcAONMEM",
+        "thumbnail": "https://i.ytimg.com/vi/KMSYcAONMEM/hqdefault.jpg",
+        "channel": {
+            "id": "UCNGAMgSoxAvQPA1716vICTw",
+            "name": "DJ Jasper",
+            "link": "https://www.youtube.com/@djJasperofficial",
+            "handle": "@djJasperofficial",
+            "verified": false,
+            "thumbnail": "https://yt3.ggpht.com/si-gSmWoCif4Fk290GeWnNSGLzTbFb91obME2cYtS5Kww7p2z4OtNsCUIhCvvJo6vxU5bhzkmQ=s0?imgmax=0"
+        },
+        "description": "best of 9ICE mixtape by DJ Jasper.",
+        "views": 5820,
+        "uploaded": "vor 1 Jahr",
+        "duration": 3001,
+        "durationString": "50:01"
+    },
+    ]
+ const channels = {
+                  "id": "UCtggJrgsA8cYpUPXG4_SvKQ",
+                  "name": "9ICEOFFICIAL",
+                  "link": "https://www.youtube.com/@9iceofficial267",
+                  "handle": "@9iceofficial267",
+                  "verified": true,
+                  "thumbnail": "https:https://yt3.googleusercontent.com/ytc/AIdro_myhF738kQYsmoqc5LDmHTH_7pF1AgQbJvGq591MEum_A=s0?imgmax=0",
+                  "description": "",
+                  "videoCount": 0,
+                  "subscribers": "0",
+                  "subscriberCount": 0
+              }
+          
+
+
+     
+
+//   ]
+//   "playlists"= [
+//       {
+//           "id": "PL0AAB0F9D6E059F31",
+//           "title": "TRADITION ALBUM Including VERSUS - 9ICE",
+//           "link": "https://www.youtube.com/playlist?list=PL0AAB0F9D6E059F31",
+//           "thumbnail": "https://i.ytimg.com/vi/HpFeGt4JPGc/hqdefault.jpg",
+//           "channel": {
+//               "id": "UC84uOwdja-XEk4QZgg__N7g",
+//               "name": "yemoshego",
+//               "link": "https://www.youtube.com/@yemoshego",
+//               "handle": "@yemoshego",
+//               "verified": false,
+//               "thumbnail": "https://www.gstatic.com/youtube/img/originals/promo/ytr-logo-for-search_160x160.png"
+//           },
+//           "videoCount": 7,
+//           "videos": [
+//               {
+//                   "id": "HpFeGt4JPGc",
+//                   "title": "9ice - Show Some Love",
+//                   "link": "https://youtu.be/HpFeGt4JPGc",
+//                   "duration": 262,
+//                   "durationString": "4:22",
+//                   "thumbnail": "https://i.ytimg.com/vi/HpFeGt4JPGc/hqdefault.jpg"
+//               },
+//               {
+//                   "id": "l6PNIrecfqY",
+//                   "title": "9ICE Gbamu Gbamu",
+//                   "link": "https://youtu.be/l6PNIrecfqY",
+//                   "duration": 244,
+//                   "durationString": "4:04",
+//                   "thumbnail": "https://i.ytimg.com/vi/l6PNIrecfqY/hqdefault.jpg"
+//               }
+//           ]
+//       },
+//       {
+//           "id": "PLYk1fcYvQNzA6gy8muSPX048EYT8TyD-F",
+//           "title": "9ice album",
+//           "link": "https://www.youtube.com/playlist?list=PLYk1fcYvQNzA6gy8muSPX048EYT8TyD-F",
+//           "thumbnail": "https://i.ytimg.com/vi/9o1akuesDGQ/hqdefault.jpg",
+//           "channel": {
+//               "id": "UCyx2YvoIMkJFajsIMCdSluw",
+//               "name": "Prince Ade",
+//               "link": "https://www.youtube.com/@daoduadebayo6065",
+//               "handle": "@daoduadebayo6065",
+//               "verified": false,
+//               "thumbnail": "https://www.gstatic.com/youtube/img/originals/promo/ytr-logo-for-search_160x160.png"
+//           },
+//           "videoCount": 87,
+//           "videos": [
+//               {
+//                   "id": "9o1akuesDGQ",
+//                   "title": "Mercy",
+//                   "link": "https://youtu.be/9o1akuesDGQ",
+//                   "duration": 235,
+//                   "durationString": "3:55",
+//                   "thumbnail": "https://i.ytimg.com/vi/9o1akuesDGQ/hqdefault.jpg"
+//               },
+//               {
+//                   "id": "tS_0lB9RV40",
+//                   "title": "Gbamu-Gbamu",
+//                   "link": "https://youtu.be/tS_0lB9RV40",
+//                   "duration": 242,
+//                   "durationString": "4:02",
+//                   "thumbnail": "https://i.ytimg.com/vi/tS_0lB9RV40/hqdefault.jpg"
+//               }
+//           ]
+//       }
+//   ];
+//   "streams"= [];
+//   ",
+//   "status" = true
